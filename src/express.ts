d17cc45e46840c353a0e57612c075c45fff6e6ee
Nick Nance <nick.nance@creditkarma.com>
2016-05-27 07:07:20 -0700

    express with parsing and validation

--- src/express.ts
+++ src/express.ts
@@ -1,8 +1,21 @@
 const express = require('express')
-import graphqlServer from './graphqlServer'
+import GraphqlServer from './graphqlServer'
 
 const app = express()
+const graphql = new GraphqlServer()
 
-app.get('/', graphqlServer)
+function handler(req, res, next) {
+    const gqlResponse = graphql.processRequst(req.body)
+
+    if (gqlResponse.errors) {
+        res.sendStatus(gqlResponse.errorCode)
+    } else {
+        res.set('Content-Type', 'application/json');
+        res.send(gqlResponse.data)
+    }
+    next()
+}
+
+app.use('/', handler)
 
 app.listen(3000)
--- src/graphqlServer.ts
+++ src/graphqlServer.ts
@@ -1,6 +1,35 @@
-import { ServerRequest, ServerResponse } from 'http'
 import * as graphql from 'graphql'
+const GraphQLValidator = require('graphql/validation')
 
-export default function(req : ServerRequest, res : ServerResponse) {
-    res.end('hello world from the graphql server')
+export interface gqlResponse {
+    errorCode?: number,
+    errors?: Array<string>,
+    data?: Object
+}
+
+export default class {
+
+    private schema: graphql.GraphQLSchema
+
+
+    constructor(schema?: graphql.GraphQLSchema) {
+        this.schema = schema
+    }
+
+    processRequst(source: string) : gqlResponse {
+        let documentAST;
+        try {
+            documentAST = graphql.parse(source);
+        } catch (syntaxError) {
+            // Return 400: Bad Request if any syntax errors errors exist.
+            return { errorCode: 400, errors: [syntaxError] };
+        }
+        // Validate AST, reporting any errors.
+        const validationErrors = GraphQLValidator.validate(this.schema, documentAST);
+
+        return JSON.stringify({
+            firstName: 'Super',
+            lastName: 'Man'
+        })
+    }
 }
